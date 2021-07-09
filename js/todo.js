c6bb0d634670b34b8c53f05d059480e96aab0431
Jay Lee <jay.lee.2280@gmail.com>
2021-07-09 15:58:17 +0900

    CSS: Global & Universal Tags

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintTodo(newTodo) {
   const span = document.createElement('span');
   span.innerText = newTodo.text;
   const button = document.createElement('button');
-  button.innerText = '❌';
+  button.innerText = 'X';
   button.addEventListener('click', deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
