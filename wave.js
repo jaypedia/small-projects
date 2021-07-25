397009c19ade310c9777d719c3ad117d2a6b0524
Jay Lee <jay.lee.2280@gmail.com>
2021-07-25 13:53:21 +0900

    class Wave

---
+++ wave.js
@@ -0,0 +1,32 @@
+import { Point } from './point.js';
+
+// Bring the coordinates of the animation I want to draw
+// Bring the width and height of the stage
+export class Wave {
+  constructor() {}
+
+  resize(stageWidth, stageHeight) {
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+
+    this.centerX = stageWidth / 2;
+    this.centerY = stageHeight / 2;
+
+    this.init();
+  }
+
+  // Execute init() to create Point()
+  init() {
+    this.point = new Point(this.centerX, this.centerY);
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = '#ff0000';
+
+    this.point.update();
+
+    ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
