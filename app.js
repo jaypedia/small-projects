d23df81df4ed9ae786e8cc620906d1d186c91194
Jay Lee <jay.lee.2280@gmail.com>
2021-08-11 10:35:10 +0900

    JS:Pick a color

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll('.jsColor');
 
 ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
 
-function stopPainting() {
-  painting = false;
-}
-
-function startPainting() {
+function startPainting(event) {
   painting = true;
 }
 
@@ -18,21 +15,30 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    // Making a path
+    ctx.beginPath(); // begin a path
+    ctx.moveTo(x, y); // Starting point
   } else {
-    ctx.lineTo(x, y);
-    ctx.stroke();
+    // Draw
+    ctx.lineTo(x, y); // Ending point
+    ctx.stroke(); // Drawing a line
   }
 }
 
-function onMouseDown(event) {
-  painting = true;
+function stopPainting(event) {
+  painting = false;
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 if (canvas) {
-  canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mouseleave', stopPainting);
 }
+
+colors.forEach((color) => color.addEventListener('click', handleColorClick));
