b73a933380f2c2e56b32d96237c2133ae1171559
Jay Lee <jay.lee.2280@gmail.com>
2021-08-12 12:12:35 +0900

    Delete button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form');
 const newListInput = document.querySelector('[data-new-list-input');
+const deleteListButton = document.querySelector('[data-delete-list-button]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
@@ -11,6 +12,15 @@ let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 listsContainer.addEventListener('click', (e) => {
   if (e.target.tagName.toLowerCase() === 'li') {
     selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
+deleteListButton.addEventListener('click', (e) => {
+  const check = confirm('Do you want to deleted this list?');
+  if (check === true) {
+    lists = lists.filter((list) => list.id !== selectedListId);
+    selectedListId = null;
     saveAndRender();
   }
 });
