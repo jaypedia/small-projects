202d85e48a7222d1e816b4ee16477b2846118c76
Jay Lee <jay.lee.2280@gmail.com>
2021-08-17 18:02:40 +0900

    trial

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const list = document.querySelector('.list');
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+const addBtn = form.querySelector('button.add');
+const listRow = document.querySelector('li.list__row');
+const deleteBtn = document.querySelector('button img');
+const item = listRow.querySelector('div.item');
 
-const ul = document.createElement('ul');
+function handleInput(e) {
+  e.preventDefault();
+  const userInput = input.value;
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+  item.innerHTML = `<span class="list__name">${userInput}</span><button class="list__delete"><img src="bucket-list.png" alt="bucket" /></button>`;
+  listRow.appendChild(item);
+  input.value = '';
+}
+
+function handleDelete() {
+  listRow.removeChild(item);
+}
+
+form.addEventListener('submit', handleInput);
+addBtn.addEventListener('click', handleInput);
+deleteBtn.addEventListener('click', handleDelete);
