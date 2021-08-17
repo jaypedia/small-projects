8b33320450a9799ee4ad70f08c1798a32be12701
Jay Lee <jay.lee.2280@gmail.com>
2021-08-17 21:28:52 +0900

    JS

--- app.js
+++ app.js
@@ -2,24 +2,40 @@ const form = document.querySelector('form');
 const input = form.querySelector('input');
 const addBtn = form.querySelector('button.add');
 const listRow = document.querySelector('li.list__row');
-const deleteBtn = document.querySelector('button img');
-const item = listRow.querySelector('div.item');
 
 function handleInput(e) {
   e.preventDefault();
   const userInput = input.value;
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-  item.innerHTML = `<span class="list__name">${userInput}</span><button class="list__delete"><img src="bucket-list.png" alt="bucket" /></button>`;
-  listRow.appendChild(item);
-  input.value = '';
+  if (userInput !== '') {
+    createItem(userInput);
+    input.value = '';
+  } else {
+    alert('What do you want to do before you die?😉');
+  }
+  input.focus();
 }
 
-function handleDelete() {
-  listRow.removeChild(item);
-  console.log('delete');
+function createItem(userInput) {
+  const items = document.createElement('div');
+  items.setAttribute('class', 'items');
+
+  const span = document.createElement('span');
+  span.setAttribute('class', 'list__name');
+  span.textContent = userInput;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'list__delete');
+  deleteBtn.innerHTML = '<img src="bucket-list.png" alt="bucket" />';
+  deleteBtn.addEventListener('click', () => {
+    listRow.removeChild(items);
+  });
+
+  items.appendChild(span);
+  items.appendChild(deleteBtn);
+  listRow.appendChild(items);
+
+  items.scrollIntoView({ block: 'center', behavior: 'smooth' });
 }
 
 form.addEventListener('submit', handleInput);
 addBtn.addEventListener('click', handleInput);
-deleteBtn.addEventListener('click', handleDelete);
