68548b2700a185c8d5dcc14ca4e09f8dc1a76229
Jay Lee <jay.lee.2280@gmail.com>
2021-08-18 09:20:41 +0900

    HTML:Re-markup

--- app.js
+++ app.js
@@ -28,11 +28,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  function checkForMatch() {}
+
   function flipCard() {
     let cardId = this.getAttribute('data-id');
     cardsChosen.push(cardArray[cardId].name);
     cardsChosenId.push(cardId);
     this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
   }
 
   createBoard();
