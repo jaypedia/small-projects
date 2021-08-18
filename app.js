6b576a5628c0a0ffada76e4e660c523122fc958e
Jay Lee <jay.lee.2280@gmail.com>
2021-08-18 13:40:47 +0900

    JS:Match,random

--- app.js
+++ app.js
@@ -13,18 +13,21 @@ document.addEventListener('DOMContentLoaded', () => {
     { name: 'milkshake', img: 'images/milkshake.png' },
     { name: 'hotdog', img: 'images/hotdog.png' },
   ];
+
+  cardArray.sort(() => 0.5 - Math.random());
   let cardsChosen = [];
   let cardsChosenId = [];
+  let cardsWon = [];
 
   const grid = document.querySelector('.grid');
   const popUp = document.querySelector('.pop-up');
   const playBtn = document.querySelector('button.game__play');
   const replayBtn = document.querySelector('.pop-up .pop-up__replay');
+  const score = document.querySelector('#score');
   const message = document.querySelector('.pop-up .pop-up__message');
 
   function playGame() {
     popUp.classList.add('hidden');
-    createBoard();
     playBtn.innerHTML = '<i class="fas fa-stop"></i>';
     playBtn.removeEventListener('click', playGame);
     playBtn.addEventListener('click', replayGame);
@@ -50,7 +53,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  function checkForMatch() {}
+  function checkForMatch() {
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    if (optionOneId === optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'images/blank.png');
+      cards[optionTwoId].setAttribute('src', 'images/blank.png');
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      cards[optionOneId].setAttribute('src', 'images/white.png');
+      cards[optionTwoId].setAttribute('src', 'images/white.png');
+      cards[optionOneId].removeEventListener('click', flipCard);
+      cards[optionTwoId].removeEventListener('click', flipCard);
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/blank.png');
+      cards[optionTwoId].setAttribute('src', 'images/blank.png');
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    score.textContent = cardsWon.length;
+
+    if (cardsWon.length === cardArray.length / 2) {
+      popUp.classList.remove('hidden');
+      message.innerHTML = 'Congratulations🎉<br>You found them all!';
+    }
+  }
 
   function flipCard() {
     let cardId = this.getAttribute('data-id');
@@ -61,4 +90,6 @@ document.addEventListener('DOMContentLoaded', () => {
       setTimeout(checkForMatch, 500);
     }
   }
+
+  createBoard();
 });
