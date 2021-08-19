597d9242856644bb2b3766e46cb6a2649fefdddd
Jay Lee <jay.lee.2280@gmail.com>
2021-08-19 21:24:29 +0900

    JS:Game Field

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const field = document.querySelector('.game__field');
+
+const ITEM_SIZE = 100;
+
 const fieldRect = field.getBoundingClientRect();
-
-const playBtn = document.querySelector('.game__button');
-
-playBtn.addEventListener('click', () => {
-  field.innerHTML = '<img src="img/carrot.png" alt="carrot">';
-  const carrot = document.querySelector('img');
-  console.dir(carrot);
-  carrot.x = 10;
-});
 
 function initGame() {
   console.log(fieldRect);
-  addItem('carrot', 5, 'img/carrot.png');
-  addItem('bug', 5, 'img/bug.png');
+  addItem('carrot', 20, 'img/carrot.png');
+  addItem('bug', 20, 'img/bug.png');
 }
 
-function addItem(className, count, imgPath) {}
+function addItem(className, count, imgPath) {
+  const x2 = fieldRect.width - ITEM_SIZE;
+  const y2 = fieldRect.height - ITEM_SIZE;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    console.dir(item);
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    item.style.position = 'absolute';
+    const x = randomNumber(x2);
+    const y = randomNumber(y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.appendChild(item);
+  }
+}
+
+function randomNumber(num) {
+  return Math.random() * num;
+}
 
 initGame();
