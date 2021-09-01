ca3d6ed11396f5dc7e4c3a3199d30e0cdc7c5ca1
Jay Lee <jay.lee.2280@gmail.com>
2021-09-01 21:56:13 +0900

    Before

---
+++ coordinates/before.js
@@ -0,0 +1,19 @@
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+window.addEventListener('mousemove', (event) => {
+  let clientX = event.clientX;
+  let clientY = event.clientY;
+  tag.innerHTML = `${clientX}px, ${clientY}px`;
+
+  vertical.style.left = `${clientX}px`;
+  horizontal.style.top = `${clientY}px`;
+
+  target.style.left = `${clientX}px`;
+  target.style.top = `${clientY}px`;
+
+  tag.style.left = `${clientX}px`;
+  tag.style.top = `${clientY}px`;
+});
