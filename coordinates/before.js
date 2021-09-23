5699d70331df21b0aadc3293c13899165f42dabf
Jay Lee <jay.lee.2280@gmail.com>
2021-09-23 21:57:55 +0900

    After

--- coordinates/before.js
+++ coordinates/before.js
@@ -3,7 +3,7 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-window.addEventListener('mousemove', (event) => {
+window.addEventListener('mousemove', event => {
   let clientX = event.clientX;
   let clientY = event.clientY;
   tag.innerHTML = `${clientX}px, ${clientY}px`;
