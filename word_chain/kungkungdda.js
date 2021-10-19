1144c415184a595e747db38f97642900003fed91
Jay <jay.lee.2280@gmail.com>
2021-10-19 09:31:20 +0900

    🎨 Add kepress event to $input

--- word_chain/kungkungdda.js
+++ word_chain/kungkungdda.js
@@ -22,13 +22,22 @@ if (number) {
       $word.textContent = word;
       const order = Number($order.textContent);
       $order.textContent = order + 1 > number ? 1 : order + 1;
+    } else if (newWord.length !== 3) {
+      alert('Check the number of the word.');
+    } else if (word[word.length - 1] !== newWord[0]) {
+      alert("The words don't match.");
     } else {
-      alert('You are wrong');
+      alert('You Lose! Try again!');
     }
     $input.value = '';
     $input.focus();
   }
 
+  $input.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+      onClick(event);
+    }
+  });
   $input.addEventListener('input', onInput);
   $button.addEventListener('click', onClick);
 }
