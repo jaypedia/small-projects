03783ebdb725c0b0846d60a4002a144cf8467741
Jay <jay.lee.2280@gmail.com>
2021-12-05 19:03:19 +0900

    Check userInput

--- baseball-game/main.js
+++ baseball-game/main.js
@@ -1,5 +1,6 @@
 const $form = document.querySelector('form');
 const $input = document.querySelector('input');
+const $tryBtn = document.querySelector('.try-button');
 const $logs = document.querySelector('.logs');
 
 const NUMBER = 4;
@@ -40,3 +41,85 @@ function getRandomNumbers2() {
 }
 console.log(getRandomNumbers2());
 */
+
+getRandomNumbers();
+
+const tries = [];
+// 사용자의 입력값이 올바른지 검사해주는 함수(중복되지 않은 숫자 4개)
+function checkUserInput() {
+  const userInput = $input.value;
+  //   checkCount(userInput);
+  //   checkNumber(userInput);
+  //   checkDuplicate(userInput);
+
+  // alert의 반환값은 undefined
+  if (userInput.length !== 4) {
+    $input.value = '';
+    return alert('숫자 4자리를 입력해 주세요.');
+  }
+  if (new Set(userInput).size !== 4) {
+    $input.value = '';
+    return alert('중복되지 않은 숫자들을 입력해 주세요.');
+  }
+  if (tries.includes(userInput)) {
+    $input.value = '';
+    return alert('이미 시도한 숫자네요.');
+  }
+  return true;
+}
+
+/* 
+function checkCount(userInput) {
+  if (userInput.length !== 4) {
+    alert('Please enter 4 numbers.');
+    return;
+  }
+}
+
+function checkNumber(userInput) {
+  if (isNaN(Number(userInput))) {
+    alert('Please enter only numbers.');
+    return;
+  }
+}
+
+function checkDuplicate(userInput) {
+  const userInputArr = userInput.split('');
+  let isDuplicate = false;
+
+  for (let i = 0; i < userInputArr.length; i++) {
+    const currentElement = userInputArr[i];
+    // 내부 반복문에서는 첫 번째 반복문에서 선택된 원소 이후의 값을 순차적으로 순회
+    for (let j = i + 1; j < userInputArr.length; j++) {
+      // 첫 번째 반복문에서 선택된 원소와 두 번째 반복문에서 선택된 원소의 값을 비교
+      // 동일한 값이 있으면 true로 설정 후 반복문 중단
+      if (currentElement === userInputArr[j]) {
+        isDuplicate = true;
+        break;
+      }
+    }
+    if (isDuplicate) {
+      alert('Numbers should not be duplicated');
+      return;
+    }
+  }
+}
+*/
+
+$form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (checkUserInput()) {
+    tries.push($input.value);
+    console.log(tries);
+    $input.value = '';
+  }
+});
+
+$tryBtn.addEventListener('click', e => {
+  e.preventDefault();
+  if (checkUserInput()) {
+    tries.push($input.value);
+    console.log(tries);
+    $input.value = '';
+  }
+});
