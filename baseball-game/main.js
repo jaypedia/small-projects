38f3af81fa23cc921c5bbce7f3c30cfed32637d4
Jay <jay.lee.2280@gmail.com>
2021-12-05 19:36:31 +0900

    Baseball game : check  Homerun

--- baseball-game/main.js
+++ baseball-game/main.js
@@ -7,21 +7,6 @@ const NUMBER = 4;
 
 // 무작위로 1~9 사이에서 중복되지 않게 숫자 4개 뽑기
 function getRandomNumbers() {
-  const answerArr = [];
-
-  while (answerArr.length < NUMBER) {
-    let randomNumber = Math.floor(Math.random() * 9 + 1);
-
-    if (answerArr[answerArr.length - 1] !== randomNumber) {
-      answerArr.push(randomNumber);
-    }
-  }
-  return answerArr;
-}
-
-/*
-// Another way to get random numbers - use index
-function getRandomNumbers2() {
   const numbers = [];
   for (let i = 0; i < 9; i++) {
     numbers.push(i + 1);
@@ -31,18 +16,18 @@ function getRandomNumbers2() {
   // 4번만 반복
   // Math.random으로 얻은 난수를 index로 이용
   // 중복되지 않게 splice 사용
-  // splice 이용으로 배열의 길이가 1씩 감소하기 때문에 Math.random에 숫자를 곱해줄 때 i만큼 빼주기 
+  // splice 이용으로 배열의 길이가 1씩 감소하기 때문에 Math.random에 숫자를 곱해줄 때 i만큼 빼주기
   for (let i = 0; i < NUMBER; i++) {
     const index = Math.floor(Math.random() * (numbers.length - i));
     answerArr.push(numbers[index]);
     numbers.splice(index, 1);
   }
-  return answerArr;
-}
-console.log(getRandomNumbers2());
-*/
 
-getRandomNumbers();
+  console.log('Answer : ', answerArr.join(''));
+  return answerArr.join('');
+}
+
+const answer = getRandomNumbers();
 
 const tries = [];
 // 사용자의 입력값이 올바른지 검사해주는 함수(중복되지 않은 숫자 4개)
@@ -106,20 +91,40 @@ function checkDuplicate(userInput) {
 }
 */
 
+function clearInput() {
+  $input.value = '';
+}
+
 $form.addEventListener('submit', e => {
   e.preventDefault();
-  if (checkUserInput()) {
-    tries.push($input.value);
-    console.log(tries);
-    $input.value = '';
+  if (!checkUserInput()) return;
+
+  if ($input.value === answer) {
+    clearInput();
+    $logs.textContent = '홈런@@@@@';
+    return;
+  }
+
+  tries.push($input.value);
+  console.log(tries);
+  clearInput();
+
+  if (tries.length >= 3) {
+    const failMessage = document.createTextNode(
+      `실패! 정답은 ${answer}입니다.`
+    );
+    $logs.appendChild(failMessage);
   }
 });
 
-$tryBtn.addEventListener('click', e => {
-  e.preventDefault();
-  if (checkUserInput()) {
-    tries.push($input.value);
-    console.log(tries);
-    $input.value = '';
-  }
-});
+// $tryBtn.addEventListener('click', e => {
+//   e.preventDefault();
+//   if (checkUserInput()) {
+//     tries.push($input.value);
+//     console.log(tries);
+//     $input.value = '';
+//   }
+//   if ($input.value === answer) {
+//     alert('홈런입니다!');
+//   }
+// });
