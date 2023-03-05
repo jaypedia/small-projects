2a76cdb6d27c45fdb99dfe324e364b75fbbaeac5
Millie <jay.lee.2280@gmail.com>
2023-03-05 12:50:30 +0900

    [#3] Feat: step 2 - implement renderMenu function
    - categoryItems 배열(상태)를 받아서 렌더링해주는 함수 구현

--- moonbucks/src/js/components/menuItem.js
+++ moonbucks/src/js/components/menuItem.js
@@ -1,7 +1,8 @@
-export const createMenuItem = (menuName) => {
-  const menuItem = document.createElement('li');
-  menuItem.className = 'menu-list-item d-flex items-center py-2';
-  menuItem.innerHTML = `
+import { $ } from '../../utils/dom.js';
+
+export const menuItemTemplate = (menuName, id) => {
+  return `
+    <li data-menu-id=${id} class="menu-list-item d-flex items-center py-2">
   <span class="w-100 pl-2 menu-name">${menuName}</span>
   <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
     수정
@@ -9,6 +10,14 @@ export const createMenuItem = (menuName) => {
   <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
     삭제
   </button>
-    `;
-  return menuItem;
+</li>
+  `;
+};
+
+export const renderMenu = (categoryItems) => {
+  const $menuList = $('#espresso-menu-list');
+  $menuList.innerHTML = categoryItems.reduce((acc, cur, idx) => {
+    const item = menuItemTemplate(cur.name, idx);
+    return acc + item;
+  }, '');
 };
