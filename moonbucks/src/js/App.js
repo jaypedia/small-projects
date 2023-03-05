c994227e6b6a5c8688c25bb08e72db83c3f93e48
Millie <jay.lee.2280@gmail.com>
2023-03-05 10:50:17 +0900

    [#2] Refactor: step 1 - separate functions

--- moonbucks/src/js/App.js
+++ moonbucks/src/js/App.js
@@ -2,41 +2,53 @@ import { $ } from '../utils/dom.js';
 import { updateMenuCount } from '../utils/count.js';
 import { createMenuItem } from './components/menuItem.js';
 
-export const App = () => {
-  const $form = $('#espresso-menu-form');
-  const $menuInput = $('#espresso-menu-name');
-  const $menuSubmitBtn = $('#espresso-menu-submit-button');
-  const $menuList = $('#espresso-menu-list');
+const $form = $('#espresso-menu-form');
+const $menuInput = $('#espresso-menu-name');
+const $menuSubmitBtn = $('#espresso-menu-submit-button');
+const $menuList = $('#espresso-menu-list');
 
+const createMenu = () => {
+  const menuInputValue = $menuInput.value;
+  if (!menuInputValue) return;
+  const menuItem = createMenuItem(menuInputValue);
+  $menuList.appendChild(menuItem);
+  updateMenuCount('create');
+  $menuInput.value = '';
+};
+
+const updateMenu = (target) => {
+  const $menuName = target.closest('li').querySelector('.menu-name');
+  const menuNameText = $menuName.textContent;
+  const updatedMenuText = prompt('Enter the new menu name', menuNameText);
+  $menuName.textContent = updatedMenuText;
+};
+
+const deleteMenu = (target) => {
+  const isConfirmed = confirm('Are you sure you want to remove the item?');
+  if (isConfirmed) {
+    target.closest('li').remove();
+    updateMenuCount('delete');
+  }
+};
+
+export const App = () => {
   const formHandler = (e) => {
     e.preventDefault();
-    const menuInputValue = $menuInput.value;
-    if (!menuInputValue) return;
-    const menuItem = createMenuItem(menuInputValue);
-    $menuList.appendChild(menuItem);
-    updateMenuCount('create');
-    $menuInput.value = '';
+    createMenu();
   };
 
   const menuItemHandler = ({ target }) => {
     if (target.classList.contains('menu-edit-button')) {
-      const $menuName = target.closest('li').querySelector('.menu-name');
-      const menuNameText = $menuName.textContent;
-      const updatedMenuText = prompt('Enter the new menu name', menuNameText);
-      $menuName.textContent = updatedMenuText;
+      updateMenu(target);
       return;
     }
     if (target.classList.contains('menu-remove-button')) {
-      const isConfirmed = confirm('Are you sure you want to remove the item?');
-      if (isConfirmed) {
-        target.closest('li').remove();
-        updateMenuCount('delete');
-      }
+      deleteMenu(target);
       return;
     }
   };
 
   $form.addEventListener('submit', formHandler);
-  $menuSubmitBtn.addEventListener('click', formHandler);
+  $menuSubmitBtn.addEventListener('click', createMenu);
   $menuList.addEventListener('click', menuItemHandler);
 };
